9fc966f3c915dbc9da6e3d169220e2161f67985e
void-echo <beihai0616@gmail.com>
2022-08-10 19:10:26 +0800

    Initial

---
+++ src/main/java/com/echo/fastdbproj/FastDbProjApplication.java
@@ -0,0 +1,13 @@
+package com.echo.fastdbproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastDbProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastDbProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/echo/fastdbproj/FastDbProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echo.fastdbproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastDbProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
